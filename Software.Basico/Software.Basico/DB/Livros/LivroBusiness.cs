90cc71a588c7bbda1c835ab43ceeb96a2d0f34dc
Pedro Henrique <PHM1080@outlook.com>
2018-11-30 18:13:17 -0200

    Base de dados adicionada; Controle de Livros Programado

---
+++ Software.Basico/Software.Basico/DB/Livros/LivroBusiness.cs
@@ -0,0 +1,35 @@
+﻿using Software.Basico.DB.Base;
+using System.Collections.Generic;
+
+namespace Software.Basico.DB.Livros
+{
+    class LivroBusiness
+    {
+        LivroDatabase db = new LivroDatabase();
+
+        public void CadastrarLivro(tb_livro dto)
+        {
+            db.CadastrarLivro(dto);
+        }
+
+        public void AlterarLivro(tb_livro dto, int idLivro)
+        {
+            db.AlterarLivro(dto, idLivro);
+        }
+
+        public void RemoverLivro(int idLivro)
+        {
+            db.RemoverLivro(idLivro);
+        }
+
+        public List<tb_livro> ListarLivros()
+        {
+            return db.ListarLivros();
+        }
+
+        public tb_livro ListarLivroPorId(int idLivro)
+        {
+            return db.ListarLivroPorId(idLivro);
+        }
+    }
+}
---
+++ Software.Basico/Software.Basico/DB/Livros/LivroDatabase.cs
@@ -0,0 +1,49 @@
+﻿using Software.Basico.DB.Base;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software.Basico.DB.Livros
+{
+    class LivroDatabase
+    {
+        AzureBiblioteca db = new AzureBiblioteca();
+
+        public void CadastrarLivro(tb_livro dto)
+        {
+            db.tb_livro.Add(dto);
+            db.SaveChanges();
+        }
+
+        public void AlterarLivro(tb_livro dto, int idLivro)
+        {
+            tb_livro func = db.tb_livro.Where(x => x.id_livro == idLivro).ToList().Single();
+
+            //Programar Alterar.
+
+            db.SaveChanges();
+        }
+
+        public void RemoverLivro(int idLivro)
+        {
+            var func = new tb_livro { id_livro = idLivro };
+            db.Entry(func).State = EntityState.Deleted;
+            db.SaveChanges();
+        }
+
+        public List<tb_livro> ListarLivros()
+        {
+            List<tb_livro> funcList = db.tb_livro.ToList();
+            return funcList;
+        }
+
+        public tb_livro ListarLivroPorId(int idLivro)
+        {
+            tb_livro func = db.tb_livro.Where(x => x.id_livro == idLivro).ToList().Single();
+            return func;
+        }
+    }
+}
