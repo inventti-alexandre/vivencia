f7a77300adab449d5ebf7c937ede1c0cd992f905
infoA35 <infoA35@INFO17>
2018-12-06 08:14:39 -0200

    Livre de Erros

--- Software.Basico/Software.Basico/Telas/Modulos/Email/frmGerEmail.cs
+++ Software.Basico/Software.Basico/Telas/Modulos/Email/frmGerEmail.cs
@@ -59,197 +59,197 @@ namespace Software.Basico.Telas.Modulos.Email
             btnEnviarDia.ForeColor = Tema.Texto;
         }
 
-        private void btnEmailDia_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                EnviarEmailDia();
+        //private void btnEmailDia_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        EnviarEmailDia();
 
-                MessageBox.Show("Emails enviados com sucesso!", "Biblioteca",
-                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //        MessageBox.Show("Emails enviados com sucesso!", "Biblioteca",
+        //            MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-                btnEnviarDia.Enabled = false;
-            }
-            catch (ArgumentException ex)
-            {
-                MessageBox.Show(ex.Message, "Biblioteca",
-                    MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Ocorreu um erro não identificado: {ex.Message}", "Biblioteca",
-                    MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-        }
+        //        btnEnviarDia.Enabled = false;
+        //    }
+        //    catch (ArgumentException ex)
+        //    {
+        //        MessageBox.Show(ex.Message, "Biblioteca",
+        //            MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show($"Ocorreu um erro não identificado: {ex.Message}", "Biblioteca",
+        //            MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //    }
+        //}
 
-        private void EnviarEmail5Dia()
-        {
-            DateTime email5dias = DateTime.Today;
-            email5dias = email5dias.AddDays(5);
+        //private void EnviarEmail5Dia()
+        //{
+        //    DateTime email5dias = DateTime.Today;
+        //    email5dias = email5dias.AddDays(5);
 
-            AzureBiblioteca db = new AzureBiblioteca();
-            List<tb_emprestimo> livro5dia = db.tb_emprestimo.Where(x => x.dt_devolucao == email5dias).ToList();
+        //    AzureBiblioteca db = new AzureBiblioteca();
+        //    List<tb_emprestimo> livro5dia = db.tb_emprestimo.Where(x => x.dt_devolucao == email5dias).ToList();
 
-            //var emails = livro5dia.Select(x => x.ds_email);
+        //    //var emails = livro5dia.Select(x => x.ds_email);
 
-            if (livro5dia.Count != 0)
-            {
-               // Send(emails);
+        //    if (livro5dia.Count != 0)
+        //    {
+        //       // Send(emails);
 
-                void Send(IEnumerable<string> Destinatarios)
-                {
-                    foreach (string destino in Destinatarios)
-                    {
-                        EmailDTO email = new EmailDTO();
-                        email.Assunto = "Faltam 5 dias!";
-                        email.DestinatarioEmail = destino;
-                        email.DestinatarioNome = "";
-                        email.Mensagem = Resources.email2;
-                        email.RemetenteSenha = "pbtadmin1234";
-                        email.RemetenteNome = "Biblioteca FREI";
-                        email.RemetenteEmail = "pb.technology.ltda@gmail.com";
+        //        void Send(IEnumerable<string> Destinatarios)
+        //        {
+        //            foreach (string destino in Destinatarios)
+        //            {
+        //                EmailDTO email = new EmailDTO();
+        //                email.Assunto = "Faltam 5 dias!";
+        //                email.DestinatarioEmail = destino;
+        //                email.DestinatarioNome = "";
+        //                email.Mensagem = Resources.email2;
+        //                email.RemetenteSenha = "pbtadmin1234";
+        //                email.RemetenteNome = "Biblioteca FREI";
+        //                email.RemetenteEmail = "pb.technology.ltda@gmail.com";
 
-                        EmailSend send = new EmailSend();
-                        send.EnviarEmail(email);
-                    }
-                }
-            }
-            else
-                throw new ArgumentException("Nenhum email a ser enviado!");
-        }
+        //                EmailSend send = new EmailSend();
+        //                send.EnviarEmail(email);
+        //            }
+        //        }
+        //    }
+        //    else
+        //        throw new ArgumentException("Nenhum email a ser enviado!");
+        //}
 
-        private void EnviarEmailDia()
-        {
-            AzureBiblioteca db = new AzureBiblioteca();
-            List<tb_emprestimo> emprestimos = db.tb_emprestimo.Where(x => x.dt_devolucao == DateTime.Today).ToList();
-            //var emails = emprestimos.Select(x => x.ds_email);
+        //private void EnviarEmailDia()
+        //{
+        //    AzureBiblioteca db = new AzureBiblioteca();
+        //    List<tb_emprestimo> emprestimos = db.tb_emprestimo.Where(x => x.dt_devolucao == DateTime.Today).ToList();
+        //    //var emails = emprestimos.Select(x => x.ds_email);
 
-            if (emprestimos.Count != 0)
-            {
-               // Send(emails);
+        //    if (emprestimos.Count != 0)
+        //    {
+        //       // Send(emails);
 
-                void Send(IEnumerable<string> Destinatarios)
-                {
-                    foreach (string destino in Destinatarios)
-                    {
-                        EmailDTO email = new EmailDTO();
-                        email.Assunto = "Dia de Devolução!";
-                        email.DestinatarioEmail = destino;
-                        email.DestinatarioNome = "";
-                        email.Mensagem = Resources.htmlEmailDia;
-                        email.RemetenteSenha = "pbtadmin1234";
-                        email.RemetenteNome = "Biblioteca FREI";
-                        email.RemetenteEmail = "pb.technology.ltda@gmail.com";
+        //        void Send(IEnumerable<string> Destinatarios)
+        //        {
+        //            foreach (string destino in Destinatarios)
+        //            {
+        //                EmailDTO email = new EmailDTO();
+        //                email.Assunto = "Dia de Devolução!";
+        //                email.DestinatarioEmail = destino;
+        //                email.DestinatarioNome = "";
+        //                email.Mensagem = Resources.htmlEmailDia;
+        //                email.RemetenteSenha = "pbtadmin1234";
+        //                email.RemetenteNome = "Biblioteca FREI";
+        //                email.RemetenteEmail = "pb.technology.ltda@gmail.com";
 
-                        EmailSend send = new EmailSend();
-                        send.EnviarEmail(email);
-                    }
-                }
-            }
-            else
-                throw new ArgumentException("Nenhum email a ser enviado!");
-        }
+        //                EmailSend send = new EmailSend();
+        //                send.EnviarEmail(email);
+        //            }
+        //        }
+        //    }
+        //    else
+        //        throw new ArgumentException("Nenhum email a ser enviado!");
+        //}
 
-        private void EnviarEmailAtrasado()
-        {
-            AzureBiblioteca db = new AzureBiblioteca();
-            List<tb_emprestimo> livroatrasado = db.tb_emprestimo.Where(x => x.dt_devolucao < DateTime.Today).ToList();
-            //var emails = livroatrasado.Select(x => x.ds_email);
+        //private void EnviarEmailAtrasado()
+        //{
+        //    AzureBiblioteca db = new AzureBiblioteca();
+        //    List<tb_emprestimo> livroatrasado = db.tb_emprestimo.Where(x => x.dt_devolucao < DateTime.Today).ToList();
+        //    //var emails = livroatrasado.Select(x => x.ds_email);
 
-            if (livroatrasado.Count != 0)
-            {
-                //Send(emails);
+        //    if (livroatrasado.Count != 0)
+        //    {
+        //        //Send(emails);
 
-                void Send(IEnumerable<string> Destinatarios)
-                {
-                    foreach (string destino in Destinatarios)
-                    {
-                        EmailDTO email = new EmailDTO();
-                        email.Assunto = "Dia de Devolução!";
-                        email.DestinatarioEmail = destino;
-                        email.DestinatarioNome = "";
-                        email.Mensagem = Resources.email3;
-                        email.RemetenteSenha = "pbtadmin1234";
-                        email.RemetenteNome = "Biblioteca FREI";
-                        email.RemetenteEmail = "pb.technology.ltda@gmail.com";
+        //        void Send(IEnumerable<string> Destinatarios)
+        //        {
+        //            foreach (string destino in Destinatarios)
+        //            {
+        //                EmailDTO email = new EmailDTO();
+        //                email.Assunto = "Dia de Devolução!";
+        //                email.DestinatarioEmail = destino;
+        //                email.DestinatarioNome = "";
+        //                email.Mensagem = Resources.email3;
+        //                email.RemetenteSenha = "pbtadmin1234";
+        //                email.RemetenteNome = "Biblioteca FREI";
+        //                email.RemetenteEmail = "pb.technology.ltda@gmail.com";
 
-                        EmailSend send = new EmailSend();
-                        send.EnviarEmail(email);
-                    }
-                }
-            }
-            else
-                throw new ArgumentException("Nenhum email a ser enviado!");
-        }
+        //                EmailSend send = new EmailSend();
+        //                send.EnviarEmail(email);
+        //            }
+        //        }
+        //    }
+        //    else
+        //        throw new ArgumentException("Nenhum email a ser enviado!");
+        //}
 
-        private void btnClose_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                if (btnEnviarDia.Enabled == true || btnEnviar5Dia.Enabled == true || btnEnviarAtrasado.Enabled == true)
-                    throw new ArgumentException("Ainda essiste lembretes a serem enviados,\nQuer mesmo fechar a janela?");
-                else
-                {
-                    Program.notificacaoEmail = false;
-                    this.Close();
-                }
-            }
-            catch (ArgumentException ex)
-            {
-                if (MessageBox.Show(ex.Message, "Biblioteca",
-                                MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
-                {
-                    this.Close();
-                }
-                else
-                    return;
-            }
-        }
+        //private void btnClose_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        if (btnEnviarDia.Enabled == true || btnEnviar5Dia.Enabled == true || btnEnviarAtrasado.Enabled == true)
+        //            throw new ArgumentException("Ainda essiste lembretes a serem enviados,\nQuer mesmo fechar a janela?");
+        //        else
+        //        {
+        //            Program.notificacaoEmail = false;
+        //            this.Close();
+        //        }
+        //    }
+        //    catch (ArgumentException ex)
+        //    {
+        //        if (MessageBox.Show(ex.Message, "Biblioteca",
+        //                        MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+        //        {
+        //            this.Close();
+        //        }
+        //        else
+        //            return;
+        //    }
+        //}
 
-        private void btnEnviar5Dia_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                EnviarEmail5Dia();
+        //private void btnEnviar5Dia_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        EnviarEmail5Dia();
 
-                MessageBox.Show("Emails enviados com sucesso!", "Biblioteca",
-                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //        MessageBox.Show("Emails enviados com sucesso!", "Biblioteca",
+        //            MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-                btnEnviar5Dia.Enabled = false;
-            }
-            catch (ArgumentException ex)
-            {
-                MessageBox.Show(ex.Message, "Biblioteca",
-                    MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Ocorreu um erro não identificado: {ex.Message}", "Biblioteca",
-                    MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-        }
+        //        btnEnviar5Dia.Enabled = false;
+        //    }
+        //    catch (ArgumentException ex)
+        //    {
+        //        MessageBox.Show(ex.Message, "Biblioteca",
+        //            MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show($"Ocorreu um erro não identificado: {ex.Message}", "Biblioteca",
+        //            MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //    }
+        //}
 
-        private void btnEnviarAtrasado_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                EnviarEmailAtrasado();
+        //private void btnEnviarAtrasado_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        EnviarEmailAtrasado();
 
-                MessageBox.Show("Emails enviados com sucesso!", "Biblioteca",
-                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //        MessageBox.Show("Emails enviados com sucesso!", "Biblioteca",
+        //            MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-                btnEnviarAtrasado.Enabled = false;
-            }
-            catch (ArgumentException ex)
-            {
-                MessageBox.Show(ex.Message, "Biblioteca",
-                    MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Ocorreu um erro não identificado: {ex.Message}", "Biblioteca",
-                    MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-        }
+        //        btnEnviarAtrasado.Enabled = false;
+        //    }
+        //    catch (ArgumentException ex)
+        //    {
+        //        MessageBox.Show(ex.Message, "Biblioteca",
+        //            MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show($"Ocorreu um erro não identificado: {ex.Message}", "Biblioteca",
+        //            MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //    }
+        //}
     }
 }
